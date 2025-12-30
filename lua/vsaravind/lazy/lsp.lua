baea7a2f98ea6b3e529ba0016426a2c47c1030a4
vsaravind01 <vsaravind01@gmail.com>
2025-12-30 19:36:27 +0530

    Update lsp with nvim 0.11 requirements

--- lua/vsaravind/lazy/lsp.lua
+++ lua/vsaravind/lazy/lsp.lua
@@ -1,145 +1,146 @@
+-- LSP on_attach
 local on_attach = function(_, _)
     require("which-key").add({
-        {
-            "<leader>rn",
-            vim.lsp.buf.rename,
-            mode="n",
-            desc="LSP Rename"
-        },
-        {
-            "<leader>ca",
-            vim.lsp.buf.code_action,
-            mode="n",
-            desc="LSP Code Action"
-        },
-        {
-            "gd",
-            vim.lsp.buf.declaration,
-            mode="n",
-            desc="LSP Definition"
-        },
-        {
-            "gD",
-            vim.lsp.buf.definitions,
-            mode="n",
-            desc="LSP Declaration"
-        },
-        {
-            "gi",
-            vim.lsp.buf.implementation,
-            mode="n",
-            desc="LSP Implementation"
-        },
+        { "<leader>rn", vim.lsp.buf.rename, desc = "LSP Rename" },
+        { "<leader>ca", vim.lsp.buf.code_action, desc = "LSP Code Action" },
+
+        { "gd", vim.lsp.buf.definition, desc = "LSP Definition" },
+        { "gD", vim.lsp.buf.declaration, desc = "LSP Declaration" },
+        { "gi", vim.lsp.buf.implementation, desc = "LSP Implementation" },
         {
             "gr",
             require("telescope.builtin").lsp_references,
-            mode="n",
-            desc="LSP References (Telescope)"
+            desc = "LSP References (Telescope)",
         },
-    })
+    }, { mode = "n" })
 end
 
+-- Plugin spec
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
         {
-            "WhoIsSethDaniel/mason-tool-installer.nvim",
+            "williamboman/mason.nvim",
             config = function()
-                require('mason-tool-installer').setup {
+                require("mason").setup()
+            end,
+        },
+        {
+            "williamboman/mason-lspconfig.nvim",
+            dependencies = { "williamboman/mason.nvim" },
+            config = function()
+                require("mason-lspconfig").setup({
+                    ensure_installed = {
+                        "lua_ls",
+                        "pyright",
+                        "pylsp",
+                        "jsonls",
+                        "gopls",
+                        "bashls",
+                        "yamlls",
+                        "marksman",
+                        "html",
+                    },
+                })
+            end,
+        },
+        {
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+            dependencies = { "williamboman/mason.nvim" },
+            config = function()
+                require("mason-tool-installer").setup({
                     ensure_installed = {
                         "stylua",
                         "black",
-                        "debugpy",
-                        "yapf",
+                        "isort",
                         "autopep8",
+                        "yapf",
+                        "flake8",
                         "pylint",
                         "mypy",
-                        "flake8",
-                        "isort",
-                    }
-                }
+                        "debugpy",
+                    },
+                })
             end,
-            dependencies = {
-                "williamboman/mason-lspconfig.nvim",
-                dependencies = {
-                    "williamboman/mason.nvim",
-                    config = function()
-                        require("mason").setup()
-                    end
-
-                },
-                config = function()
-                    require("mason-lspconfig").setup({
-                        ensure_installed = {
-                            'lua_ls',
-                            'html',
-                            'bashls',
-                            'jsonls',
-                            'marksman',
-                            'yamlls',
-                            'pyright',
-                            'gopls'
-                        }
-                    })
-                end
-            },
         },
         {
             "stevearc/conform.nvim",
-            opts = {}
-        }
+            opts = {},
+        },
     },
+
     config = function()
-        local conform = require("conform")
-        conform.setup({
+        -- Formatting
+        require("conform").setup({
             formatters_by_ft = {
                 lua = { "stylua" },
-                python = { "yapf", "flake8", "isort", "black", "autopep8" },
-                go = { "goimports", "gofmt" }
-            }
+                python = { "isort", "black", "autopep8", "yapf" },
+                go = { "goimports", "gofmt" },
+            },
         })
 
-        local lspconfig = require("lspconfig")
-
+        -- Capabilities
         local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+        capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-        lspconfig.pyright.setup({
+        -- LSP servers (Neovim 0.11 API)
+
+        vim.lsp.config("pyright", {
             capabilities = capabilities,
-            on_attach = on_attach
+            on_attach = on_attach,
         })
-        lspconfig.jsonls.setup({
+
+        vim.lsp.config("pylsp", {
             capabilities = capabilities,
-            on_attach = on_attach
+            on_attach = on_attach,
+            settings = {
+                pylsp = {
+                    configurationSources = { "flake8" },
+                    plugins = {
+                        flake8 = {
+                            enabled = true,
+                            config = "setup.cfg",
+                        },
+                        pycodestyle = { enabled = false },
+                    },
+                },
+            },
         })
-        lspconfig.gopls.setup({
+
+        vim.lsp.config("jsonls", {
             capabilities = capabilities,
-            on_attach = on_attach
+            on_attach = on_attach,
         })
-        lspconfig.lua_ls.setup ({
+
+        vim.lsp.config("gopls", {
+            capabilities = capabilities,
+            on_attach = on_attach,
+        })
+
+        vim.lsp.config("lua_ls", {
+            capabilities = capabilities,
+            on_attach = on_attach,
             on_init = function(client)
                 local path = client.workspace_folders[1].name
-                if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+                if vim.uv.fs_stat(path .. "/.luarc.json")
+                    or vim.uv.fs_stat(path .. "/.luarc.jsonc")
+                then
                     return
                 end
 
-                client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-                    runtime = {
-                        version = 'LuaJIT'
-                    },
-                    workspace = {
-                        checkThirdParty = false,
-                        library = {
-                            vim.env.VIMRUNTIME
-                        }
+                client.config.settings.Lua = vim.tbl_deep_extend(
+                    "force",
+                    client.config.settings.Lua,
+                    {
+                        runtime = { version = "LuaJIT" },
+                        workspace = {
+                            checkThirdParty = false,
+                            library = { vim.env.VIMRUNTIME },
+                        },
                     }
-                })
+                )
             end,
-            on_attach = on_attach,
-            settings = {
-                Lua = {}
-            }
+            settings = { Lua = {} },
         })
-    end
+    end,
 }
-
